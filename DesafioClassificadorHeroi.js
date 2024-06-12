841a5ef0872f1a4bc4aac4a327e8a4c0195148af
João Pedro Zampoli <107592828+JoaoPedroZampoli@users.noreply.github.com>
2024-06-11 23:45:35 -0300

    Add files via upload

---
+++ DesafioClassificadorHeroi.js
@@ -0,0 +1,29 @@
+let NomeHeroi = "Steve Rogers"
+let XPHeroi = "9000"
+
+if (XPHeroi < 1000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Ferro")
+}
+else if(XPHeroi >= 1001 && XPHeroi <= 2000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Bronze")
+}
+else if(XPHeroi >= 2001 && XPHeroi <= 5000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Prata")
+}
+else if(XPHeroi >= 5001 && XPHeroi <= 7000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Ouro")
+}
+else if(XPHeroi >= 7001 && XPHeroi <= 8000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Platina")
+}
+else if(XPHeroi >= 8001 && XPHeroi <= 9000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Ascendente")
+}
+else if(XPHeroi >= 9001 && XPHeroi <= 10000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Imortal")
+}
+else if(XPHeroi >= 10001){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Radiante")
+}
+
+
